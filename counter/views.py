9d5e7ceb08e608c318f4895b3ea8cf2bc77bb32f
n-mini <nmininmini533@gmail.com>
2021-09-03 10:35:29 +0900

    vue.js仕様に変更

--- counter/views.py
+++ counter/views.py
@@ -1,15 +1,5 @@
 from django.views.generic.edit import FormView
+from django.shortcuts import render
 
-from . import forms
-
-class Index(FormView):
-
-    form_class = forms.TextForm
-    template_name = 'index.html'
-
-    def form_valid(self, form):
-        data = form.cleaned_data
-        text = len(data['text'])
-
-        ctxt = self.get_context_data(text=text,form=form)
-        return self.render_to_response(ctxt)
+def top(request):
+    return render(request, 'index.html')
